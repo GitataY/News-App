93c8d18bcac0f7115d2586471334e441a9c88cfc
Wamuyu Gitata <yvonnewamuyug@gmail.com>
2024-04-30 20:04:55 +0300

    completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
+import { useState } from "react"
 import Navbar from "./Components/Navbar"
 import Newsboard from "./Components/Newsboard"
 
 
 const App = () => {
+
+  const [category, setCategory] = useState('general');
   return (
     <>
-      <Navbar />
-      <Newsboard />
+      <Navbar setCategory={setCategory} />
+      <Newsboard category={category} />
     </>
   )
 }
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,38 +1,36 @@
-const Navbar = () => {
+const Navbar = ({ setCategory }) => {
     return (
         <nav className="navbar navbar-expand-lg bg-body-tertiary" data-bs-theme="dark">
             <div className="container-fluid">
-                <a className="navbar-brand" href="#"><span className="badge bg-light text-dark fs-4">News Trail</span>
-                </a>
-                <button
-                    className="navbar-toggler"
-                    type="button"
-                    data-bs-toggle="collapse"
-                    data-bs-target="#navbarNavAltMarkup"
-                    aria-controls="navbarNavAltMarkup"
-                    aria-expanded="false"
-                    aria-label="Toggle navigation"
-                >
+                <a className="navbar-brand" href="#"><span className="badge bg-light text-dark fs-4">News Trail</span></a>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
-                <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-                    <div className="navbar-nav">
-                        <a className="nav-link active" aria-current="page" href="#">
-                            Entertainment
-                        </a>
-                        <a className="nav-link" href="#">
-                            Sports
-                        </a>
-                        <a className="nav-link" href="#">
-                            Technology
-                        </a>
-                        <a className="nav-link" href="#">
-                            Health
-                        </a>
-                    </div>
+                <div className="collapse navbar-collapse" id="navbarNav">
+                    <ul className="navbar-nav">
+                        <li className="nav-item">
+                            <div className="nav-link" onClick={() => setCategory("technology")}>Technology</div>
+                        </li>
+                        <li className="nav-item">
+                            <div className="nav-link" onClick={() => setCategory("Business")}>Business</div>
+                        </li>
+                        <li className="nav-item">
+                            <div className="nav-link" onClick={() => setCategory("Health")}>Health</div>
+                        </li>
+                        <li className="nav-item">
+                            <div className="nav-link" onClick={() => setCategory("Science")}>Science</div>
+                        </li>
+                        <li className="nav-item">
+                            <div className="nav-link" onClick={() => setCategory("Sports")}>Sports</div>
+                        </li>
+                        <li className="nav-item">
+                            <div className="nav-link" onClick={() => setCategory("Entertainment")}>Entertainment</div>
+                        </li>
+                    </ul>
                 </div>
             </div>
         </nav>
+
     );
 };
 
